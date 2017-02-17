bcd258cb141d1e3c94e976f0c242b6669ca323bb
RobertVCampbell <robert.v.campbell@gmail.com>
2017-02-16 21:06:22 -0600

    Create EmployeeArr.java
    An array to handle a collection of Employee objects

---
+++ EmployeeArr.java
@@ -0,0 +1,20 @@
+/*Created by:	Robert Campbell
+ *Created on:	02/16/17
+ *Purpose:		To handle an array of Employee objects
+ *					+ Sort in alphabetic order
+ *						- First name
+ *						- Last name
+ *					+ Sort by hours scheduled
+ *					+ Sort by priority flag*/
+
+public class EmployeeArr {
+	private Employee[] roster;
+	private int first, last;
+	private int numElems;
+	private int priStart, priEnd;
+	
+	
+	
+	
+	
+}//End of class
